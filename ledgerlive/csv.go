f304b7fddceeaa8d371e06c14a3e115664771340
Fisca Facile <fiscafacile@protonmail.com>
2021-05-01 13:56:12 +0200

    Optimize LedgerLive Fee agregation

--- ledgerlive/csv.go
+++ ledgerlive/csv.go
@@ -49,38 +49,49 @@ func (ll *LedgerLive) ParseCSV(reader io.Reader, cat category.Category) (err err
 				tx.AccountName = r[6]
 				tx.AccountXpub = r[7]
 				ll.CsvTXs = append(ll.CsvTXs, tx)
-				// Fill TXsByCategory
-				if tx.Type == "IN" {
-					t := wallet.TX{Timestamp: tx.Date, Note: "LedgerLive CSV " + tx.AccountName + " : " + tx.Hash + " -> " + tx.AccountXpub}
-					t.Items = make(map[string]wallet.Currencies)
-					t.Items["To"] = append(t.Items["To"], wallet.Currency{Code: tx.Currency, Amount: tx.Amount})
+			}
+		}
+		for _, tx := range ll.CsvTXs {
+			// Fill TXsByCategory
+			if tx.Type == "IN" {
+				t := wallet.TX{Timestamp: tx.Date, Note: "LedgerLive CSV " + tx.AccountName + " : " + tx.Hash + " -> " + tx.AccountXpub}
+				t.Items = make(map[string]wallet.Currencies)
+				t.Items["To"] = append(t.Items["To"], wallet.Currency{Code: tx.Currency, Amount: tx.Amount})
+				if !tx.Fees.IsZero() {
 					t.Items["Fee"] = append(t.Items["Fee"], wallet.Currency{Code: tx.Currency, Amount: tx.Fees})
-					if is, desc, val, curr := cat.IsTxCashIn(tx.Hash); is {
-						t.Note += " crypto_purchase " + desc
-						t.Items["From"] = append(t.Items["From"], wallet.Currency{Code: curr, Amount: val})
-						ll.TXsByCategory["CashIn"] = append(ll.TXsByCategory["CashIn"], t)
-					} else {
-						ll.TXsByCategory["Deposits"] = append(ll.TXsByCategory["Deposits"], t)
-					}
-				} else if tx.Type == "OUT" {
+				}
+				if is, desc, val, curr := cat.IsTxCashIn(tx.Hash); is {
+					t.Note += " crypto_purchase " + desc
+					t.Items["From"] = append(t.Items["From"], wallet.Currency{Code: curr, Amount: val})
+					ll.TXsByCategory["CashIn"] = append(ll.TXsByCategory["CashIn"], t)
+				} else {
+					ll.TXsByCategory["Deposits"] = append(ll.TXsByCategory["Deposits"], t)
+				}
+			} else if tx.Type == "OUT" {
+				if !tx.Fees.Equal(tx.Amount) { // ignore Fee associated to other OUT, will be found later
 					t := wallet.TX{Timestamp: tx.Date, Note: "LedgerLive CSV " + tx.AccountName + " : " + tx.AccountXpub + " -> " + tx.Hash}
 					t.Items = make(map[string]wallet.Currencies)
-					t.Items["Fee"] = append(t.Items["Fee"], wallet.Currency{Code: tx.Currency, Amount: tx.Fees})
-					if tx.Amount.Sub(tx.Fees).IsZero() {
-						ll.TXsByCategory["Fees"] = append(ll.TXsByCategory["Fees"], t)
-					} else {
-						t.Items["From"] = append(t.Items["From"], wallet.Currency{Code: tx.Currency, Amount: tx.Amount.Sub(tx.Fees)})
-						if is, desc, val, curr := cat.IsTxCashOut(tx.Hash); is {
-							t.Note += " crypto_payment " + desc
-							t.Items["To"] = append(t.Items["To"], wallet.Currency{Code: curr, Amount: val})
-							ll.TXsByCategory["CashOut"] = append(ll.TXsByCategory["CashOut"], t)
-						} else {
-							ll.TXsByCategory["Withdrawals"] = append(ll.TXsByCategory["Withdrawals"], t)
+					if !tx.Fees.IsZero() {
+						t.Items["Fee"] = append(t.Items["Fee"], wallet.Currency{Code: tx.Currency, Amount: tx.Fees})
+					}
+					for _, tx2 := range ll.CsvTXs {
+						if tx2.Hash == tx.Hash &&
+							tx2.Type == "OUT" &&
+							tx2.Fees.Equal(tx2.Amount) {
+							t.Items["Fee"] = append(t.Items["Fee"], wallet.Currency{Code: tx2.Currency, Amount: tx2.Fees})
 						}
 					}
-				} else {
-					log.Println("Unmanaged ", tx)
+					t.Items["From"] = append(t.Items["From"], wallet.Currency{Code: tx.Currency, Amount: tx.Amount.Sub(tx.Fees)})
+					if is, desc, val, curr := cat.IsTxCashOut(tx.Hash); is {
+						t.Note += " crypto_payment " + desc
+						t.Items["To"] = append(t.Items["To"], wallet.Currency{Code: curr, Amount: val})
+						ll.TXsByCategory["CashOut"] = append(ll.TXsByCategory["CashOut"], t)
+					} else {
+						ll.TXsByCategory["Withdrawals"] = append(ll.TXsByCategory["Withdrawals"], t)
+					}
 				}
+			} else {
+				log.Println("Unmanaged ", tx)
 			}
 		}
 	}
