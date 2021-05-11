710e0829c97c8d0b60219e04e1cdee5869255809
Baptiste Roux <rouxbaptiste@outlook.com>
2021-05-11 21:07:56 +0200

    Add configuration file feature Copy config.example.yml to config.yml to test this feature CLI args override the config file, multiple args can be called to include multiple CSV

---
+++ cfg/config.go
@@ -0,0 +1,206 @@
+package cfg
+
+import (
+	"log"
+	"os"
+
+	"github.com/spf13/pflag"
+	"gopkg.in/yaml.v3"
+)
+
+type API struct {
+	Key    string `yaml:"key"`
+	Secret string `yaml:"secret"`
+}
+
+// Blockchains
+type BlockchainConfig struct {
+	Addresses []string `yaml:"addresses"`
+	CSV       []string `yaml:"csv"`
+	JSON      string   `yaml:"json"`
+}
+
+type Blockchains struct {
+	ADA BlockchainConfig `yaml:"ADA"`
+	BTC BlockchainConfig `yaml:"BTC"`
+	BTG BlockchainConfig `yaml:"BTG"`
+	ETH BlockchainConfig `yaml:"ETH"`
+}
+
+// Exchanges
+type CSV struct {
+	All          []string `yaml:"all"`
+	Staking      []string `yaml:"staking"`
+	Supercharger []string `yaml:"supercharger"`
+	Trades       []string `yaml:"trades"`
+	Transfers    []string `yaml:"transfers"`
+}
+
+type ExchangeConfig struct {
+	CSV  CSV    `yaml:"csv"`
+	API  API    `yaml:"api"`
+	JSON string `yaml:"json"`
+}
+
+type Exchanges struct {
+	Binance       ExchangeConfig `yaml:"binance"`
+	Bitfinex      ExchangeConfig `yaml:"bitfinex"`
+	Bittrex       ExchangeConfig `yaml:"bittrex"`
+	CdcApp        ExchangeConfig `yaml:"cdc-app"`
+	CdcEx         ExchangeConfig `yaml:"cdc-exchange"`
+	Coinbase      ExchangeConfig `yaml:"coinbase"`
+	HitBTC        ExchangeConfig `yaml:"hitbtc"`
+	Kraken        ExchangeConfig `yaml:"kraken"`
+	LocalBitcoins ExchangeConfig `yaml:"localbitcoins"`
+	Revolut       ExchangeConfig `yaml:"revolut"`
+}
+
+// Options
+type Options struct {
+	Bcd             bool   `yaml:"bcd"`
+	Bch             bool   `yaml:"bch"`
+	BinanceExtended bool   `yaml:"binance-extended"`
+	Btg             bool   `yaml:"btg"`
+	Check           bool   `yaml:"check"`
+	CurrencyFilter  string `yaml:"curr-filter"`
+	Date            string `yaml:"date"`
+	Debug           bool   `yaml:"debug"`
+	Display2086     bool   `yaml:"display-2086"`
+	Exact           bool   `yaml:"exact"`
+	Export2086      bool   `yaml:"export-2086"`
+	Export3916      bool   `yaml:"export-3916"`
+	ExportStock     bool   `yaml:"export-stock"`
+	Lbtc            bool   `yaml:"lbtc"`
+	Location        string `yaml:"location"`
+	Native          string `yaml:"native"`
+	Stats           bool   `yaml:"stats"`
+	TxsCategory     string `yaml:"txs-categ"`
+	TxsDisplay      string `yaml:"txs-display"`
+}
+
+// Tools
+type Tools struct {
+	CoinAPI   API `yaml:"coinapi"`
+	CoinLayer API `yaml:"coinlayer"`
+	EtherScan API `yaml:"etherscan"`
+}
+
+// Wallets
+type WalletConfig struct {
+	CSV CSV `yaml:"csv"`
+}
+
+type Wallets struct {
+	LedgerLive WalletConfig `yaml:"ledgerlive"`
+	MyCelium   WalletConfig `yaml:"mycelium"`
+}
+
+type Config struct {
+	Blockchains Blockchains `yaml:"blockchains"`
+	Exchanges   Exchanges   `yaml:"exchanges"`
+	Options     Options     `yaml:"options"`
+	Tools       Tools       `yaml:"tools"`
+	Wallets     Wallets     `yaml:"wallets"`
+}
+
+func loadFile() *Config {
+	config := &Config{}
+	configPath := "config.yml"
+	// Check that file exist and is readable
+	s, err := os.Stat(configPath)
+	if err != nil {
+		log.Printf("Error while reading the configuration file, '%s' not loaded", configPath)
+		return config
+	}
+	if s.IsDir() {
+		log.Printf("'%s' is a directory, no file loaded", configPath)
+		return config
+	}
+	// Open config file
+	file, err := os.Open(configPath)
+	if err != nil {
+		return config
+	}
+	defer file.Close()
+	// Load configuration from file
+	d := yaml.NewDecoder(file)
+	if err := d.Decode(&config); err != nil {
+		return config
+	}
+	return config
+}
+
+func LoadConfig() (*Config, error) {
+	// Load configuration from file
+	config := loadFile()
+	// Configure the default options
+	if config.Options.Date == "" {
+		config.Options.Date = "2021-01-01T00:00:00"
+	}
+	if config.Options.Location == "" {
+		config.Options.Location = "Europe/Paris"
+	}
+	if config.Options.Native == "" {
+		config.Options.Native = "EUR"
+	}
+
+	// Override configuration from CLI
+	// General Options
+	pflag.StringVar(&config.Options.Date, "date", config.Options.Date, "Date Filter")
+	pflag.StringVar(&config.Options.Location, "location", config.Options.Location, "Date Filter Location")
+	pflag.StringVar(&config.Options.Native, "native", config.Options.Native, "Native Currency for consolidation")
+	pflag.BoolVarP(&config.Options.Stats, "stats", "s", config.Options.Stats, "Display accounts stats")
+	// Debug
+	pflag.BoolVarP(&config.Options.Debug, "debug", "d", config.Options.Debug, "Debug Mode (only for devs)")
+	pflag.BoolVarP(&config.Options.Check, "check", "c", config.Options.Check, "Check and Display consistency")
+	pflag.StringVar(&config.Options.CurrencyFilter, "currency-filter", config.Options.CurrencyFilter, "Currencies to be filtered in Transactions Display (comma separated list)")
+	pflag.BoolVar(&config.Options.Debug, "exact", config.Options.Debug, "Display exact amount (no rounding)")
+	pflag.StringVar(&config.Options.TxsDisplay, "txs-display", config.Options.TxsDisplay, "Display Transactions By Category : Exchanges|Deposits|Withdrawals|CashIn|CashOut|etc")
+	// Sources
+	pflag.StringVar(&config.Options.TxsCategory, "txs-categ", config.Options.TxsCategory, "Transactions Categories CSV file")
+	pflag.StringVar(&config.Tools.CoinAPI.Key, "coinapi-key", config.Tools.CoinAPI.Key, "CoinAPI Key (https://www.coinapi.io/pricing?apikey)")
+	pflag.StringVar(&config.Tools.CoinLayer.Key, "coinlayer-key", config.Tools.CoinLayer.Key, "CoinLayer Key (https://coinlayer.com/product)")
+	pflag.StringSliceVar(&config.Blockchains.BTC.CSV, "btc-addresses-csv", config.Blockchains.BTC.CSV, "Bitcoin Addresses CSV files")
+	pflag.StringSliceVar(&config.Blockchains.BTC.Addresses, "btc-addresses", config.Blockchains.BTC.Addresses, "Bitcoin Addresses")
+	pflag.BoolVar(&config.Options.Bcd, "bcd", config.Options.Bcd, "Detect Bitcoin Diamond Fork")
+	pflag.BoolVar(&config.Options.Bch, "bch", config.Options.Bch, "Detect Bitcoin Cash Fork")
+	pflag.BoolVar(&config.Options.Btg, "btg", config.Options.Btg, "Detect Bitcoin Gold Fork")
+	pflag.BoolVar(&config.Options.Lbtc, "lbtc", config.Options.Lbtc, "Detect Lightning Bitcoin Fork")
+	pflag.StringVar(&config.Blockchains.BTG.JSON, "btg-txs", config.Blockchains.BTG.JSON, "Bitcoin Gold Transactions JSON file")
+	pflag.StringSliceVar(&config.Blockchains.ETH.CSV, "eth-addresses-csv", config.Blockchains.ETH.CSV, "Ethereum Addresses CSV file")
+	pflag.StringSliceVar(&config.Blockchains.ETH.Addresses, "eth-addresses", config.Blockchains.ETH.Addresses, "Ethereum Addresses")
+	pflag.StringVar(&config.Tools.EtherScan.Key, "etherscan-apikey", config.Tools.EtherScan.Key, "Etherscan API Key (https://etherscan.io/myapikey)")
+	pflag.StringSliceVar(&config.Exchanges.Binance.CSV.All, "binance", config.Exchanges.Binance.CSV.All, "Binance CSV file")
+	pflag.BoolVar(&config.Options.BinanceExtended, "binance-extended", config.Options.BinanceExtended, "Use Binance CSV file extended format")
+	pflag.StringSliceVar(&config.Exchanges.Bitfinex.CSV.All, "bitfinex", config.Exchanges.Bitfinex.CSV.All, "Bitfinex CSV file")
+	pflag.StringVar(&config.Exchanges.Bittrex.API.Key, "bittrex-api-key", config.Exchanges.Bittrex.API.Key, "Bittrex API key")
+	pflag.StringVar(&config.Exchanges.Bittrex.API.Secret, "bittrex-api-secret", config.Exchanges.Bittrex.API.Secret, "Bittrex API secret")
+	pflag.StringSliceVar(&config.Exchanges.Bittrex.CSV.All, "bittrex", config.Exchanges.Bittrex.CSV.All, "Bittrex CSV file")
+	pflag.StringSliceVar(&config.Exchanges.HitBTC.CSV.Trades, "hitbtc-trades", config.Exchanges.HitBTC.CSV.Trades, "HitBTC Trades CSV file")
+	pflag.StringSliceVar(&config.Exchanges.HitBTC.CSV.Transfers, "hitbtc-transactions", config.Exchanges.HitBTC.CSV.Transfers, "HitBTC Transfers CSV file")
+	pflag.StringVar(&config.Exchanges.HitBTC.API.Key, "hitbtc-api-key", config.Exchanges.HitBTC.API.Key, "HitBTC API Key")
+	pflag.StringVar(&config.Exchanges.HitBTC.API.Secret, "hitbtc-api-secret", config.Exchanges.HitBTC.API.Secret, "HitBTC API Secret")
+	pflag.StringSliceVar(&config.Exchanges.Coinbase.CSV.All, "coinbase", config.Exchanges.Coinbase.CSV.All, "Coinbase CSV file")
+	pflag.StringSliceVar(&config.Exchanges.CdcApp.CSV.All, "cdc-app-crypto", config.Exchanges.CdcApp.CSV.All, "Crypto.com App Crypto Wallet CSV file")
+	pflag.StringVar(&config.Exchanges.CdcEx.API.Key, "cdc-ex-api-key", config.Exchanges.CdcEx.API.Key, "Crypto.com Exchange API Key")
+	pflag.StringVar(&config.Exchanges.CdcEx.API.Secret, "cdc-ex-api-secret", config.Exchanges.CdcEx.API.Secret, "Crypto.com Exchange Secret Key")
+	pflag.StringVar(&config.Exchanges.CdcEx.JSON, "cdc-ex-exportjs", config.Exchanges.CdcEx.JSON, "Crypto.com Exchange JSON file from json-exporter.js")
+	pflag.StringSliceVar(&config.Exchanges.CdcEx.CSV.Transfers, "cdc-ex-transfer", config.Exchanges.CdcEx.CSV.Transfers, "Crypto.com Exchange Deposit/Withdrawal CSV file")
+	pflag.StringSliceVar(&config.Exchanges.CdcEx.CSV.Staking, "cdc-ex-stake", config.Exchanges.CdcEx.CSV.Staking, "Crypto.com Exchange Stake CSV file")
+	pflag.StringSliceVar(&config.Exchanges.CdcEx.CSV.Supercharger, "cdc-ex-supercharger", config.Exchanges.CdcEx.CSV.Supercharger, "Crypto.com Exchange Supercharger CSV file")
+	pflag.StringVar(&config.Exchanges.Kraken.API.Key, "kraken-api-key", config.Exchanges.Kraken.API.Key, "Kraken API key")
+	pflag.StringVar(&config.Exchanges.Kraken.API.Secret, "kraken-api-secret", config.Exchanges.Kraken.API.Secret, "Kraken API secret")
+	pflag.StringSliceVar(&config.Exchanges.Kraken.CSV.All, "kraken", config.Exchanges.Kraken.CSV.All, "Kraken CSV file")
+	pflag.StringSliceVar(&config.Wallets.LedgerLive.CSV.All, "ledgerlive", config.Wallets.LedgerLive.CSV.All, "LedgerLive CSV file")
+	pflag.StringSliceVar(&config.Exchanges.LocalBitcoins.CSV.Trades, "lb-trade", config.Exchanges.LocalBitcoins.CSV.Trades, "Local Bitcoin Trade CSV file")
+	pflag.StringSliceVar(&config.Exchanges.LocalBitcoins.CSV.Transfers, "lb-transfer", config.Exchanges.LocalBitcoins.CSV.Transfers, "Local Bitcoin Transfer CSV file")
+	pflag.StringSliceVar(&config.Wallets.MyCelium.CSV.All, "mycelium", config.Wallets.MyCelium.CSV.All, "MyCelium CSV file")
+	pflag.StringSliceVar(&config.Exchanges.Revolut.CSV.All, "revolut", config.Exchanges.Revolut.CSV.All, "Revolut CSV file")
+	// Output
+	pflag.BoolVar(&config.Options.Display2086, "2086_display", config.Options.Display2086, "Display Cerfa 2086")
+	pflag.BoolVar(&config.Options.Export2086, "2086", config.Options.Export2086, "Export Cerfa 2086 to 2086.xlsx")
+	pflag.BoolVar(&config.Options.Export3916, "3916", config.Options.Export3916, "Export Cerfa 3916 to 3916.xlsx")
+	pflag.BoolVar(&config.Options.ExportStock, "stock", config.Options.ExportStock, "Export stock balances to stock.xlsx")
+	pflag.Parse()
+	return config, nil
+}
