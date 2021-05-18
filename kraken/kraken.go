fa5a5cf12a6ceebac811150fe5da7dc6de2ed38e
Fisca Facile <fiscafacile@protonmail.com>
2021-05-18 16:07:30 +0200

    Remove file

--- kraken/kraken.go
+++ kraken/kraken.go
@@ -1,6 +1,8 @@
 package kraken
 
 import (
+	"strings"
+
 	"github.com/fiscafacile/CryptoFiscaFacile/source"
 	"github.com/fiscafacile/CryptoFiscaFacile/wallet"
 )
@@ -42,4 +44,47 @@ func (kr *Kraken) GetAPIAllTXs() {
 
 func (kr *Kraken) WaitFinish() error {
 	return <-kr.done
+}
+
+func ReplaceAssets(assetToReplace string) string {
+	assetRplcr := strings.NewReplacer(
+		"ADA.S", "ADA",
+		"ATOM.S", "ATOM",
+		"DOT.S", "DOT",
+		"ETH2", "ETH",
+		"ETH2.S", "ETH",
+		"EUR.HOLD", "EUR",
+		"EUR.M", "EUR",
+		"FLOW.S", "FLOW",
+		"FLOWH", "FLOW",
+		"FLOWH.S", "FLOW",
+		"KAVA.S", "KAVA",
+		"KFEE", "FEE",
+		"KSM.S", "KSM",
+		"USD.HOLD", "USD",
+		"USD.M", "USD",
+		"XBT", "BTC",
+		"XBT.M", "BTC",
+		"XETC", "ETC",
+		"XETH", "ETH",
+		"XLTC", "LTC",
+		"XMLN", "MLN",
+		"XREP", "REP",
+		"XTZ", "XTZ",
+		"XTZ.S", "XTZ",
+		"XXBT", "BTC",
+		"XXDG", "DOGE",
+		"XXLM", "XLM",
+		"XXMR", "XMR",
+		"XXRP", "XRP",
+		"XZEC", "ZEC",
+		"ZAUD", "AUD",
+		"ZCAD", "CAD",
+		"ZEUR", "EUR",
+		"ZGBP", "GBP",
+		"ZJPY", "JPY",
+		"ZRX", "ZRX",
+		"ZUSD", "USD",
+	)
+	return assetRplcr.Replace(assetToReplace)
 }
